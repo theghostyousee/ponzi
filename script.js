294c39cf739903a81d81e0e52a3aa381cab2e82b
theghostyousee <patrickserise174@gmail.com>
2023-09-16 21:07:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+
+document.getElementById("muerto").addEventListener("click", function () {
+  window.location.href = "https://www.youtube.com";
+});
